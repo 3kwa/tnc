a96d7c6b0773e1996ae5aff8a436d67b0287a168
Eugene Van den Bulke <eugene@hollersydney.com.au>
2010-11-10 11:21:19 +1100

    form handling

--- templates/__init__.py
+++ templates/__init__.py
@@ -2,8 +2,10 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 
 
-lookup = TemplateLookup(directories=['templates'])
+lookup = TemplateLookup(directories=['templates'], default_filters=['h'])
 
 def serve_template(filename, **data):
+    man = {'form_errors' : {}, 'form_values' : {}}
+    man.update(data)
     template = lookup.get_template(filename)
-    return template.render(**data)
+    return template.render(**man)

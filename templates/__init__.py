e16ea2c68f012e8ade2638dd1ff43889880f1353
Eugene Van den Bulke <eugene@hollersydney.com.au>
2010-12-03 12:15:03 +1100

    utf-8

--- templates/__init__.py
+++ templates/__init__.py
@@ -2,7 +2,9 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 
 
-lookup = TemplateLookup(directories=['templates'], default_filters=['h'])
+lookup = TemplateLookup(directories=['templates'],
+                        default_filters=['h'],
+                        output_encoding = 'utf-8')
 
 def serve_template(filename, **data):
     man = {'form_errors' : {}, 'form_values' : {}}
